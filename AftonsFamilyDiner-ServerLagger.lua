e01926ed9cdd3792269fc11bb9ce3654dbcfd64f
Lua Lover <96401271+XxxXLuaLover@users.noreply.github.com>
2023-06-28 20:09:40 -0700

    Update AftonsFamilyDiner-ServerLagger.lua

--- AftonsFamilyDiner-ServerLagger.lua
+++ AftonsFamilyDiner-ServerLagger.lua
@@ -1,7 +1,12 @@
 local RunService = game:GetService("RunService")
+for i,v in pairs(game.Players:GetChildren()) do
+if v.Name == game.Players.LocalPlayer.Name then
+ServerBye = v.Name
 RunService.Heartbeat:Connect(function(step)
 game:GetService("ReplicatedStorage").NightmareFoxy.Base:FireServer(table.unpack({
-    [1] = workspace.stinkynga,
-    [2] = workspace.stinkynga.Humanoid,
+    [1] = workspace[ServerBye],
+    [2] = workspace[ServerBye].Humanoid,
 }))
+            end
+        end
 end)
